1e8d33cfe6efc29a32d78cd5aa91c8c0d244ae75
Amoy02 <pattersonamoy@gmail.com>
2020-05-03 22:05:36 -0500

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = '\xee\xca\xb9.Q\xf7\xbd\xfe\xda|X\xfb\x93\x98\xdc\x15
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://yourusername:yourpassword@localhost/databasename'
 
 #Heroku Daatabase
-app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://posgkpzjfnremm:6b1a61f9e55afc2019b5452cbba6eb98c7b7a258a920ef0766a8989fd5b8a16c@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d5ric48c6j7mlf'
 #app.config['UPLOAD_FOLDER']= join(dirname(realpath(__file__)), 'static/uploads')
 
 
